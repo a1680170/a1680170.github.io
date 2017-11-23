d725707af26a5204914c9adb0cc585d14dae229e
Mason Wong <Seong6232@gmail.com>
2017-11-23 10:15:44 +0800

    Update

--- help/js/help.js
+++ help/js/help.js
@@ -1,11 +1,4 @@
 $(function () {
-    $(document).ready(function(){
-        var currentLocation = location.search;
-        var curr = currentLocation.substring(1, currentLocation.length);
-        $("#" + curr + "-panel").show();
-        $(".panel-priv:not(#" + curr + "-panel)").hide();
-    });
-
     $(window).on("resize", function () {
         pheigt();
     });
@@ -17,34 +10,14 @@ $(function () {
         $(".helpmain").add("#helpmain-main").css("min-height", windowheight + "px");
     }
 
-    $(window).scroll(function () {
-        var scrollTop = $(this).scrollTop();
-        var pNav = ".helpmain .nav dl";
-        var nheight = $(pNav).height();
-        var pheight = $(".helpmain").height();
-        if (scrollTop >= 104) {
-            if (pheight + 154 - scrollTop <= nheight) {
-                $(pNav).removeClass("fixed");
-                $(pNav).addClass("bottom");
-                return false;
-            } else {
-                $(pNav).removeClass("bottom");
-                $(pNav).addClass("fixed");
-            }
-        } else {
-            $(pNav).removeClass("fixed");
-        }
-    });
-
-
     var nav = "#helpmain-nav dd";
     $(nav).click(function () {
         var obj = $(this).attr("data");
-        $(nav).removeClass("on");
+        $(nav).not($(this)).removeClass("on");
         $(this).addClass("on");
         $(".panel-priv").hide();
         $("#" + obj + "-panel").show();
-        window.location.href = window.location.pathname + "?" + obj;
+        // window.location.href = window.location.pathname + "?" + obj;
     });
 
 
@@ -63,45 +36,45 @@ $(function () {
     /**End-Submain**/
 
 
-    var objurl = window.location.search.substr(1);
-    if (objurl == "") {
-        $("#deposit-panel").show();
-    } else {
-        $("#" + objurl + "-panel").show();
-        $(nav).removeClass("on").each(function (index, element) {
-            var obj = $(this).attr("data");
-            if (obj == objurl) {
-                $(this).addClass("on");
-            }
-
-        });
-
-
-    }
+    // var objurl = window.location.search.substr(1);
+    // if (objurl == "") {
+    //     $("#deposit-panel").show();
+    // } else {
+    //     $("#" + objurl + "-panel").show();
+    //     $(nav).removeClass("on").each(function (index, element) {
+    //         var obj = $(this).attr("data");
+    //         if (obj == objurl) {
+    //             $(this).addClass("on");
+    //         }
+    //
+    //     });
+    //
+    //
+    // }
 
     /**Submain**/
-    var subobjurl = window.location.search.substr(1);
-    if (subobjurl == "deposit") {
-        $(".panel-sub").hide();
-        //$("#subsport-panel").show();
-        //
-    } else {
-        $("#" + subobjurl + "-panel").show();
-        $(subnav).removeClass("on").each(function (index, element) {
-            var subobj = $(this).attr("data");
-            if (subobj == subobjurl) {
-                $(this).addClass("on");
-            }
-
-        });
-        if (subobjurl == "games") {
-            $("#subsport").addClass("on");
-            /** 默认显示 体育问题**/
-            $("#subsport-panel").show();
-            /** 默认显示 体育问题**/
-
-        }
-    }
+    // var subobjurl = window.location.search.substr(1);
+    // if (subobjurl == "deposit") {
+    //     $(".panel-sub").hide();
+    //     //$("#subsport-panel").show();
+    //     //
+    // } else {
+    //     $("#" + subobjurl + "-panel").show();
+    //     $(subnav).removeClass("on").each(function (index, element) {
+    //         var subobj = $(this).attr("data");
+    //         if (subobj == subobjurl) {
+    //             $(this).addClass("on");
+    //         }
+    //
+    //     });
+    //     if (subobjurl == "games") {
+    //         $("#subsport").addClass("on");
+    //         /** 默认显示 体育问题**/
+    //         $("#subsport-panel").show();
+    //         /** 默认显示 体育问题**/
+    //
+    //     }
+    // }
     /**Submain**/
 
 
