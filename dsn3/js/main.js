f6922f7bfb20d7eb3338a1cc4d241e398ee9087c
Mason Wong <Seong6232@gmail.com>
2018-01-11 16:24:24 +0800

    Added many pages

--- dsn3/js/main.js
+++ dsn3/js/main.js
@@ -12,10 +12,12 @@ $(document).ready(()=>{
 	getCurrentGame();
 
 	var scene = document.getElementById('scene');
-	var parallaxInstance = new Parallax(scene, {
-	  relativeInput: true
-	});
-
+	if(scene){
+		var parallaxInstance = new Parallax(scene, {
+		  relativeInput: true
+		});
+	}
+	
 	var mySwiper = new Swiper ('.swiper-container', {
 	    // Optional parameters
 	    direction: 'horizontal',
@@ -53,6 +55,9 @@ $(document).ready(()=>{
 	// Scroll reveal
 	window.sr = ScrollReveal();
 
+
+	//Index 
+	// selector, duration, delay, distance, origin
 	scrollReveal('.line.left', 1000, 0, 100, 'right');
 	scrollReveal('.line.right', 1000, 0, 100, 'left');
 	scrollReveal('.service-footer', 600, 0, 345, 'bottom');
@@ -62,8 +67,17 @@ $(document).ready(()=>{
 	scrollReveal('.current-game', 600, 900, 345, 'right');
 	scrollReveal('.games-container .button.go', 600, 1000, 345, 'bottom');
 
+	//Mobile
+	scrollReveal('.phone-wrapper .mobile1', 600, 0, 345, 'bottom');
+	scrollReveal('.phone-wrapper .mobile2', 600, 400, 100, 'right');
+	scrollReveal('.phone-wrapper .mobile3', 600, 400, 100, 'left');
+	scrollReveal('.phone-wrapper .mobile4', 600, 800, 200, 'right');
+	scrollReveal('.phone-wrapper .mobile5', 600, 800, 200, 'left');
+	scrollReveal('.phone-wrapper .mickey', 600, 800, 200, 'left');
+
 	stragerScrollReveal('.service-wrapper > div', 600, 200, 300, 'bottom');
 	stragerScrollReveal('.service .service-item', 600, 100, 300, 'bottom');
+	stragerScrollReveal('.service.mobile .service-item', 600, 100, 300, 'right');
 	stragerScrollReveal('.game-list .game-item', 600, 50, 300, 'bottom');
 	stragerScrollReveal('.partner li', 600, 100, 200, 'bottom');	
 
@@ -80,6 +94,9 @@ $(document).ready(()=>{
 			$(window).unbind('scroll');
 		}
 	});
+
+	tabContentItemToggle('.sub-content-item-header', '.sub-content-item-content');
+	tabSwitchContentToggle('.tab-container.left-panel .tab-switch ul li a', '.tab-container.left-panel .tab-content .sub-content');
 })
 
 
@@ -141,4 +158,23 @@ function increaseServiceAmount(selector, max, speed, type){
 			clearInterval(increaseServiceAmountInterval);
 		}
 	}, speed);
+}
+
+function tabContentItemToggle(selector, siblings){
+	$(selector).click(function(e){
+		e.preventDefault();
+		$(this).siblings(siblings).slideToggle();
+	})
+}
+
+function tabSwitchContentToggle(selector, target){
+	$(selector).click(function(e){
+		e.preventDefault();
+		$(this).parent("li").addClass("active");
+		$(selector).not($(this)).parent("li").removeClass("active");
+		var index = $(this).parent("li").index() + 1;
+
+		$(target + ':nth-child(' + index + ')').addClass("active");
+		$(target + ':not(:nth-child(' + index + '))').removeClass("active");
+	})
 }
